de18e988ba9945696fccd9a202aaae58a60db124
alexpikuta <olexandr.pikuta@gmail.com>
2018-11-03 11:18:13 +0200

    Develop single page

--- src/components/Currency.vue
+++ src/components/Currency.vue
@@ -1,6 +1,38 @@
 <template>
   <div>
-    <p>{{ item.name }}</p>
+    <v-layout row>
+      <v-flex xs12 sm8 offset-sm2 md8 offset-md2>
+        <v-list >
+          <v-list-tile>
+            <v-list-tile-content>
+              <v-list-tile-title>id</v-list-tile-title>
+              <v-list-tile-sub-title class="text-xs-right">{{ item.itemId }}</v-list-tile-sub-title>
+            </v-list-tile-content>
+          </v-list-tile>
+
+          <v-list-tile>
+            <v-list-tile-content>
+              <v-list-tile-title>Name</v-list-tile-title>
+              <v-list-tile-sub-title class="text-xs-right">{{ item.name }}</v-list-tile-sub-title>
+            </v-list-tile-content>
+          </v-list-tile>
+
+          <v-list-tile>
+            <v-list-tile-content>
+              <v-list-tile-title>Location</v-list-tile-title>
+              <v-list-tile-sub-title class="text-xs-right">{{ item.location }}</v-list-tile-sub-title>
+            </v-list-tile-content>
+          </v-list-tile>
+
+          <v-list-tile>
+            <v-list-tile-content>
+              <v-list-tile-title>Currency</v-list-tile-title>
+              <v-list-tile-sub-title class="text-xs-right">{{ item.currency }}</v-list-tile-sub-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </v-list>
+      </v-flex>
+    </v-layout>
   </div>
 </template>
 
@@ -11,9 +43,6 @@
       item () {
         const id = this.id
         return this.$store.getters.currencyById(id)
-      },
-      loading () {
-        return this.$store.getters.loading
       }
     }
   }
